4d730ec9f4c350ed2d3889a52e78935390bb1d72
JJ <JJ@jdotjdot.com>
2014-03-19 21:05:26 -0400

    final updates and rearranging

--- BashWeather.sh
+++ BashWeather.sh
@@ -1,117 +1,166 @@
 #!/bin/bash
 
-# while getopts ":c:l:u:" opt
-# do
-#     case $opt in
-#         c  )   # default character to display if no weather, leave empty for none
-#             local c=${OPTARG}
-#         l  )   # supply location instead of using internet
-#             local l={$OPTARG}
-#         u  )   # how often to update weather in seconds
-#             local u={$OPTARG}
-#         \? )
-#             echo "Invalid option: -$OPTARG" >&2
-#             ;;
-#         :  )
-#             echo "Option -$OPTARG requires an argument." >&2
-#             exit 1
-#             ;;
-#     esac
-# done
 
-
-
-
-# switch for default char
-# switch for using internet location or your own location
 
 ### This needs to be fixes b/c RunLocateMe breaks if there's no internet
 # If online, do weather
-# function check_internet {
-#     if [ -z $BASHWEATHER_FIRSTRUN ] ; then
-#       BASHWEATHER_FIRSTRUN="run already"
-#        # check if there is internet
-#       wget -q --tries=10 --timeout=20 http://google.com
-#       if [[ $? -eq 0 ]]; then
-#           local INTERNET=true
-#       else
-#           local INTERNET=false
-#       fi
-#     fi
-# }
-############
-
-
-local NOW=$(date +%s)
-
-# 10800 is 3 hours in Unix time--so we download the weather again after 3 hours
-# 3600 is 1 hour
-if [ -z $LAST_TIME_CHECKED_WEATHER ] ; then # on startup
-
-  read LAT LON <<<$(~/Coding/BashWeather/RunLocateMe -f "{LAT} {LON}")
-  local RESPONSEHOLDER=$(curl -s "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON" 2>/dev/null)
-  #echo $RESPONSEHOLDER
-
-  LAST_TIME_CHECKED_WEATHER=$NOW
-  local UPDATED=""
-  local ASSIGN_AGAIN=true
-
-elif [ $LAST_TIME_CHECKED_WEATHER -lt $(($NOW - 10800)) ] ; then # because would be int rather than empty string
-
- read LAT LON <<<$(~/Coding/BashWeather/RunLocateMe -f "{LAT} {LON}")
- local RESPONSEHOLDER=$(curl -s "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON" 2>/dev/null)
- #echo $RESPONSEHOLDER
-
- local UPDATED=" (updated weather)" # only adds for later updates, not for prompt initialization
- LAST_TIME_CHECKED_WEATHER=$NOW
- local ASSIGN_AGAIN=true
-
-else
-    local UPDATED=""
-fi
-
-if [ $ASSIGN_AGAIN ] ; then
-  local WEATHERCODE=$(echo $RESPONSEHOLDER | grep -o -e '"weather":[^[]*\[[^{]*{[^}]*"id": *[0-9]\{1,3\}' | tail -c 4)
-  local SUNRISE=$(echo $RESPONSEHOLDER | grep -o -e '"sunrise":[0-9]\{10\}' | tail -c 11)
-  local SUNSET=$(echo $RESPONSEHOLDER | grep -o -e '"sunset":[0-9]\{10\}' | tail -c 11)
-
-    if [ -n $WEATHERCODE ] ; then
-          if [ ${WEATHERCODE:0:1} -eq "2" ] ; then
-              WEATHERCHAR=☔︎
-          elif [ ${WEATHERCODE:0:1} -eq "3" ] ; then
-              WEATHERCHAR=☂
-          elif [ ${WEATHERCODE:0:1} -eq "5" ] ; then
-              WEATHERCHAR=☔︎
-          elif [ ${WEATHERCODE:0:1} -eq "6" ] ; then
-              WEATHERCHAR=☃
-          elif [ ${WEATHERCODE:0:1} -eq "8" ] ; then
-              if [ $NOW -lt $SUNRISE ] ; then
-                WEATHERCHAR=☽
-              elif [ $NOW -gt $SUNRISE -a $NOW -lt $SUNSET ] ; then
-                WEATHERCHAR=☀︎;
-              elif [ $NOW -gt $SUNSET ] ; then
-                WEATHERCHAR=☽
-              fi
-          else
-              WEATHERCHAR=‽ # if no weathercode make interrobang
-          fi
-    else
-        WEATHERCHAR=‽ # if internet dead so no WEATHERCODE
+function check_internet {
+    if [ -z "$BASHWEATHER_FIRSTRUN" ] ; then
+      BASHWEATHER_FIRSTRUN="run already"
+       # check if there is internet
+      wget -q --tries=10 --timeout=20 http://google.com
+      if [[ $? -eq 0 ]]; then
+          # local INTERNET=true
+          return 0
+      else
+          # local INTERNET=false
+          return 1
+      fi
     fi
-else
-    # get rid of $UPDATED from $WEATHERCHAR
-    if [ "${WEATHERCHAR:${#WEATHERCHAR} - 17}" == "(updated weather)" ]; then
-        WEATHERCHAR="${WEATHERCHAR:0:1}"
+}
+
+function getResponse {
+   local URL="http://api.openweathermap.org/data/2.5/weather?"
+
+   if [ -n "$l" ] ; then
+   # if user is supplying their location
+      if [ "$l" != "ip" ] ; then
+        local RESPONSEHOLDER=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=$l" 2>/dev/null)
+      else
+        local TEMPHOLDER=$(curl -s "http://freegeoip.net/json/" 2>/dev/null)
+        LAT=$(echo $TEMPHOLDER | grep -o -e '"latitude":[0-9\.\-]\+' | grep -o -e '[0-9\.\-]\+$')
+        LON=$(echo $TEMPHOLDER | grep -o -e '"longitude":[0-9\.\-]\+' | grep -o -e '[0-9\.\-]\+$')
+        local RESPONSEHOLDER=$(curl -s "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON" 2>/dev/null)
+      fi
+   else
+      check_internet
+      if [[ $? -eq 0 ]] ; then
+        read LAT LON <<<$($(sourceDirectory)/RunLocateMe -f "{LAT} {LON}")
+        local RESPONSEHOLDER=$(curl -s "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON" 2>/dev/null)
+      fi
+   fi
+
+   echo "$RESPONSEHOLDER"
+}
+
+function sourceDirectory {
+  # Also resolves symlinks
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  echo "$DIR"
+}
+
+function runWeather {
+
+  local OPTIND
+  while getopts ":c:l:u:s:" opt ; do
+      case "$opt" in
+          c  )   # default character to display if no weather, leave empty for none
+              c="$OPTARG"
+              ;;
+          l  )   # supply city name instead of using internet or "ip" for ip address
+              l="$OPTARG"
+              ;;
+          u  )   # how often to update weather in seconds
+              u="$OPTARG"
+              ;;
+          s  )   # weather update alert string to supply, if any
+              s="$OPTARG"
+              ;;
+          h  )
+              # echo the help file
+              ;;
+          \? )
+              echo "Invalid option: -$OPTARG" >&2
+              ;;
+          :  )
+              echo "Option -$OPTARG requires an argument." >&2
+              exit 1
+              ;;
+      esac
+  done
+  shift $((OPTIND-1))
+
+  # defaults
+  if [ -z "$u" ] ; then u=10800 ; fi
+  if [ -z "$c" ] ; then c=‽ ; fi
+
+
+  local NOW=$(date +%s)
+
+  # echo $LAST_TIME_CHECKED_WEATHER
+
+  # 10800 is 3 hours in Unix time--so we download the weather again after 3 hours
+  # 3600 is 1 hour
+  if [ -z "$LAST_TIME_CHECKED_WEATHER" ] ; then # on startup
+
+      local RESPONSEHOLDER=$(getResponse)
+
+      LAST_TIME_CHECKED_WEATHER=$NOW
+      local UPDATED=""
+      local ASSIGN_AGAIN=true
+
+  elif [ "$LAST_TIME_CHECKED_WEATHER" -lt $((NOW - u)) ] ; then # because would be int rather than empty string
+
+     local RESPONSEHOLDER=$(getResponse)
+
+     if [ -n "$s" ] ; then
+       local UPDATED="$s" # only adds for later updates, not for prompt initialization
+     fi
+     LAST_TIME_CHECKED_WEATHER=$NOW
+     local ASSIGN_AGAIN=true
+
+  else
+      local UPDATED=""
+  fi
+
+  # ASSIGN_AGAIN=
+
+  if [ $ASSIGN_AGAIN ] ; then
+    local WEATHERCODE=$(echo "$RESPONSEHOLDER" | grep -o -e '"weather":[^[]*\[[^{]*{[^}]*"id": *[0-9]\{1,3\}' | tail -c 4)
+    local SUNRISE=$(echo "$RESPONSEHOLDER" | grep -o -e '"sunrise":[0-9]\{10\}' | tail -c 11)
+    local SUNSET=$(echo "$RESPONSEHOLDER" | grep -o -e '"sunset":[0-9]\{10\}' | tail -c 11)
+
+      if [[ -n $WEATHERCODE ]] ; then
+            if [ "${WEATHERCODE:0:1}" -eq "2" ] ; then
+                WEATHERCHAR=☔︎
+            elif [ "${WEATHERCODE:0:1}" -eq "3" ] ; then
+                WEATHERCHAR=☂
+            elif [ "${WEATHERCODE:0:1}" -eq "5" ] ; then
+                WEATHERCHAR=☔︎
+            elif [ "${WEATHERCODE:0:1}" -eq "6" ] ; then
+                WEATHERCHAR=☃
+            elif [ "${WEATHERCODE:0:1}" -eq "8" ] ; then
+                if [ "$NOW" -lt "$SUNRISE" ] ; then
+                  WEATHERCHAR=☽
+                elif [ "$NOW" -gt "$SUNRISE" -a "$NOW" -lt "$SUNSET" ] ; then
+                  WEATHERCHAR=☀︎;
+                elif [ "$NOW" -gt "$SUNSET" ] ; then
+                  WEATHERCHAR=☽
+                fi
+            else
+                WEATHERCHAR="$c" # if no weathercode make interrobang
+            fi
+      else
+          WEATHERCHAR="$c" # if internet dead so no WEATHERCODE
+      fi
+  else
+    if [ -n "$s" ] ; then
+        # get rid of $UPDATED from $WEATHERCHAR
+        if [ "${WEATHERCHAR:${#WEATHERCHAR} - ${#s}}" == "$s" ] ; then
+            WEATHERCHAR="${WEATHERCHAR:0:1}"
+        fi
     fi
-fi
+  fi
 
-# echo $UPDATED
-WEATHERCHAR=$(echo -n "$WEATHERCHAR" && echo "$UPDATED" | sed 's/ *$//')
-# echo $WEATHERCHAR
+  WEATHERCHAR=$(echo -n "$WEATHERCHAR" && echo " $UPDATED" | sed 's/ *$//')
+}
 
-# if [[ ! -z $UPDATED ]] ; then
-#     WEATHERCHAR=$WEATHERCHAR$UPDATED
-# fi
+runWeather "$@"
 
-#echo "UPDATED: "$UPDATED
-#echo "WEATHERCHAR: "$WEATHERCHAR
+# WEATHERCHAR=$(echo -n "$WEATHERCHAR" && echo " $UPDATED" | sed 's/ *$//')
--- README.md
+++ README.md
@@ -2,3 +2,38 @@ BashWeather
 ===========
 
 A Bash script to add regularly updated localized weather icons to your bash prompt
+
+Program: BashWeather
+Author: J.J.
+Email: JJ@jdotjdot.com
+Version 1.0
+
+This script makes use of the following tools:
++  [LocateMe](https://github.com/netj/LocateMe) for Mac OS X by Robert Harder
+    *  Includes both the source code as well as the pre-compiled XCode project
++ [OpenWeatherMap](http://openweathermap.org)'s excellent weather API
++ [Shellcheck.net](http://www.shellcheck.net/) -- excellent Bash debugging tool!
+
+##USAGE
+BashWeather -h gives the following:
+
+USAGE:
+    Add `. [/path/to/]BashWeather.sh [options]` to your `.bashrc`,
+    then include `$WEATHERCHAR` somewhere in your bash `$PROMPT` variable.
+    If you plan to use the provided `RunLocateMe` binary that makes use of Mac OS X's geolocation feature, make sure that it is located in the same directory as `BashWeather.sh`.
+
+OPTIONS:
+ + `-c <character>` - default character to be displayed in your prompt should the weather not be available for any reason.  Default character is the interrobang (`‽`)
+ + `-l [<city and country> | ip]` - switch to instead supply your city and country in a string for location checking (e.g., `"London, UK"`).
+     * To have BashWeather check your location via your IP address instead, supply `-l ip`.
+     * Default is to use `LocateMe`.
+ + `-u <integer>` - how often, in seconds, to wait between weather updates.  Default is 10800 (3 hours).
+ + `-s <string>` - a string, like `"(weather updated)"`, to display only when the weather has just been updated.  No default.
+ + `-h` - display the help.
+
+
+BashWeather will create the following global variables in Bash:
++ `LAST_TIME_CHECKED_WEATHER` - to store the last time the weather was updated
++ `LAT`
++ `LON`
++ `WEATHERCHAR` - this is the final output of the script, a variable containing the appropriate weather character for you to include in your bash prompt as you see fit
