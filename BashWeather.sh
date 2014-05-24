ab88e999aba60a64e06161cd35187ce1d76e3690
JJ <JJ@jdotjdot.com>
2014-05-23 20:28:35 -0400

    make ip default

--- BashWeather.sh
+++ BashWeather.sh
@@ -22,22 +22,25 @@ function getResponse {
    local URL="http://api.openweathermap.org/data/2.5/weather?"
 
    if [ -n "$l" ] ; then
-   # if user is supplying their location
-      if [ "$l" != "ip" ] ; then
+   # if user supplies location or specifies LocateMe
+      if [ $(echo "$l" | tr '[:upper:]' '[:lower:]') != "locateme" ] ; then
         local RESPONSEHOLDER=$(curl --connect-timeout $t -s "http://api.openweathermap.org/data/2.5/weather?q=$l" 2>/dev/null)
       else
-        local TEMPHOLDER=$(curl --connect-timeout $t -s "http://freegeoip.net/json/" 2>/dev/null)
-        LAT=$(echo $TEMPHOLDER | grep -o -e '"latitude":[0-9\.\-]\+' | grep -o -e '[0-9\.\-]\+$')
-        LON=$(echo $TEMPHOLDER | grep -o -e '"longitude":[0-9\.\-]\+' | grep -o -e '[0-9\.\-]\+$')
-        local RESPONSEHOLDER=$(curl --connect-timeout $t -s "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON" 2>/dev/null)
+        # Using LocateMe
+        check_internet
+        if [[ $? -eq 0 ]] ; then
+          read LAT LON <<<$($(sourceDirectory)/RunLocateMe -f "{LAT} {LON}")
+          local RESPONSEHOLDER=$(curl --connect-timeout $t -s "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON" 2>/dev/null)
+        fi
       fi
    else
-      check_internet
-      if [[ $? -eq 0 ]] ; then
-        read LAT LON <<<$($(sourceDirectory)/RunLocateMe -f "{LAT} {LON}")
-        local RESPONSEHOLDER=$(curl --connect-timeout $t -s "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON" 2>/dev/null)
-      fi
+    # default is by IP address
+    local TEMPHOLDER=$(curl --connect-timeout $t -s "http://freegeoip.net/json/" 2>/dev/null)
+    LAT=$(echo $TEMPHOLDER | grep -o -e '"latitude":[0-9\.\-]\+' | grep -o -e '[0-9\.\-]\+$')
+    LON=$(echo $TEMPHOLDER | grep -o -e '"longitude":[0-9\.\-]\+' | grep -o -e '[0-9\.\-]\+$')
+    local RESPONSEHOLDER=$(curl --connect-timeout $t -s "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON" 2>/dev/null)
    fi
+
 
    echo "$RESPONSEHOLDER"
 }
@@ -62,7 +65,7 @@ function runWeather {
           c  )   # default character to display if no weather, leave empty for none
               c="$OPTARG"
               ;;
-          l  )   # supply city name instead of using internet or "ip" for ip address
+          l  )   # supply city name instead of using internet or "LocateMe" for RunLocateMe
               l="$OPTARG"
               ;;
           u  )   # how often to update weather in seconds
@@ -71,28 +74,28 @@ function runWeather {
           s  )   # weather update alert string to supply, if any
               s="$OPTARG"
               ;;
-	  t  )   # http timeout
-	      t="$OPTARG"
-	      ;;
+	        t  )   # http timeout
+      	      t="$OPTARG"
+      	      ;;
           h  )
               # echo the help file
 	      local HELP=$(cat <<"EOF"
 USAGE:
     Add `. [/path/to/]BashWeather.sh [options]` to your `.bashrc`,
-    then include `$WEATHERCHAR` below somewhere in your bash `$PROMPT` variable.
+    then include `$WEATHERCHAR` in your bash `$PROMPT` variable somewhere below that.
     If you plan to use the provided `RunLocateMe` binary that makes use of Mac OS X\'s geolocation feature, make sure that it is located in the same directory as `BashWeather.sh`.
 
 OPTIONS:
 + `-c <character>` - default character to be displayed in your prompt should the weather not be available for any reason.  Default character is the dollar sign ($).  Please note that this will NOT become a hash (#) when root.
- + `-l [<city and country> | ip]` - switch to instead supply your city and country in a string for location checking (e.g., `"London, UK"`).
-     * To have BashWeather check your location via your IP address instead, supply `-l ip`.
-     * Default is to use `LocateMe`.
+ + `-l [<city, country> | LocateMe]` - switch to instead supply your city and country in a string for location checking (e.g., `"London, UK"`).
+     * To have BashWeather check your location via the Mac built-in utility, use `-l LocateMe`.
+     * Default is to use your IP address.  Note that this will be inaccurate if using a proxy.
  + `-u <integer>` - how often, in seconds, to wait between weather updates.  Default is 10800 (3 hours).
  + `-s <string>` - a string, like `"(weather updated)"`, to display only when the weather has just been updated.  No default.
  + `-t <integer>` - timeout, in seconds, for any http requests made by BashWeather.  Make this value smaller if you want a shorter delay when no internet is available or is too slow.  Default is 1.
  + `-h` - display the help.
 EOF
-) 
+)
 	      echo "$HELP"
 	      return 1
               ;;
@@ -161,7 +164,7 @@ EOF
                   WEATHERCHAR=☽
                 fi
             else
-	        WEATHERCHAR="$c" # if no weathercode make default character ($) 
+	        WEATHERCHAR="$c" # if no weathercode make default character ($)
             fi
       else
           WEATHERCHAR="$c" # if internet dead so no WEATHERCODE
